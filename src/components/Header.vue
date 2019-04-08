9df23fad2147c3770555a519c2ab78017037838c
see91 <isee_wang@outlook.com>
2019-04-08 17:09:13 +0800

    ⚙️ Header&Banner

---
+++ src/components/Header.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id='header'>
+    <div class='_w'>
+      <router-link
+        to='/'
+        class='logo'
+      />
+      <ul class='nav'>
+        <router-link
+          tag='li'
+          v-for="(l,index) in nav"
+          :to='l.path'
+          :key='index'
+          :class="{_active:l.path===path}"
+        >{{l.name}}</router-link>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      path: null,
+      nav: [
+        {
+          path: '/',
+          name: '首页'
+        },
+        {
+          path: '/product',
+          name: '公司产品'
+        },
+        {
+          path: '/advantage',
+          name: '公司优势'
+        },
+        {
+          path: '/industry',
+          name: '服务行业'
+        },
+        {
+          path: '/contact',
+          name: '联系方式'
+        }
+      ]
+    }
+  },
+  mounted () {
+    this.path = this.$route.path
+  }
+}
+</script>
+
+<style scoped lang="less">
+.flex {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+@A: 100%;
+#header {
+  .flex();
+  position: relative;
+  z-index: 1;
+  width: @A;
+  height: 80px;
+  ._w {
+    .flex();
+    width: 80%;
+    height: @A;
+    justify-content: space-between;
+    .logo {
+      width: 152px;
+      height: 50px;
+      background: url("~@/assets/logo.png") no-repeat;
+    }
+    .nav {
+      .flex();
+      height: @A;
+      li {
+        .flex();
+        margin-right: 46px;
+        height: @A;
+        color: #fff;
+        cursor: pointer;
+      }
+      ._active {
+        border-bottom: 2px solid #ffffff;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,170 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <section class='banner'>
+      <div class='banner-top-area'>
+        <p class='t-color t-first'><span class='t-first-mark'>分布式</span>智能价值网络，让科技更安全</p>
+        <p class='t-color'>利用区块链与人工智能，让计算更快捷，存储更安全，交易更隐私</p>
+        <div class='banner-fun'>
+          <div>
+            Get started
+          </div>
+          <div>
+            Learn more
+          </div>
+        </div>
+      </div>
+      <ul class='banner-card'>
+        <li
+          v-for="l in introduction"
+          :key='l.bg'
+        >
+          <i class='introduction-bg' :class='setClass(l.bg)' />
+          <p>{{l.desc}}</p>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: 'Home',
+  data () {
+    return {
+      introduction: [
+        {
+          bg: 'd1',
+          desc: '国际知名院士加盟'
+        },
+        {
+          bg: 'd2',
+          desc: '多项分布式、智能运算和区块链等核心技术'
+        },
+        {
+          bg: 'd3',
+          desc: '首批入驻的高新技术企业'
+        }
+      ]
+    }
+  },
+  methods: {
+    setClass (key) {
+      return `${key}_bg`
+    }
   }
 }
 </script>
+
+<style lang="less" scoped>
+.flex {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.t-color {
+  color: #fff;
+  font-size: 18px;
+}
+.size{
+  background-size: 100% 100%;
+}
+#home {
+  position: relative;
+  top: -80px;
+  .banner {
+    .flex();
+    height: 720px;
+    padding-top: 80px;
+    flex-direction: column;
+    .banner-top-area {
+      .flex();
+      height: 30%;
+      flex-direction: column;
+      justify-content: space-around;
+      .t-first {
+        font-size: 36px;
+        .t-first-mark {
+          font-size: 50px;
+          font-weight: bold;
+          color: rgba(237, 128, 45, 1);
+        }
+      }
+      .banner-fun {
+        .flex();
+        div {
+          .flex();
+          width: 180px;
+          height: 50px;
+          color: #fff;
+          border-radius: 6px;
+          cursor: pointer;
+          &:nth-child(1) {
+            margin-right: 40px;
+            background-color: #ed802d;
+          }
+          &:nth-child(2) {
+            border: 1px solid #fff;
+          }
+        }
+      }
+    }
+    .banner-card {
+      .flex();
+      position: absolute;
+      width: 80%;
+      height: 150px;
+      bottom: -50px;
+      background-color: #fff;
+      border-radius: 10px;
+      li {
+        .flex();
+        width: 30%;
+        height: 50%;
+        overflow: hidden;
+        padding: 0 30px;
+        vertical-align: middle;
+        border-right: 2px solid rgba(237, 128, 45, 1);
+        .introduction-bg {
+          min-width: 35px;
+          height: 38px;
+          margin-right: 20px;
+        }
+        .d1_bg {
+          background: url("../assets/d1.png") no-repeat;
+          .size();
+        }
+        .d2_bg {
+          background: url("../assets/d2.png") no-repeat;
+          .size();
+        }
+        .d3_bg {
+          background: url("../assets/d3.png") no-repeat;
+          .size();
+        }
+        p {
+          font-size: 20px;
+          font-weight: 500;
+          line-height: 25px;
+          color: rgba(51, 51, 51, 1);
+        }
+        &:nth-child(3){
+          border:none;
+        }
+      }
+    }
+    &::after {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      z-index: -1;
+      height: 100%;
+      filter: blur(8px);
+      background: url("~@/assets/banner.png") no-repeat;
+      background-size: 100% 100%;
+    }
+  }
+}
+</style>
